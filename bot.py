3e3c33ba0902d1b4f5b1c64c6c90d52b7cb5d5bc
Senya <wamir4054@gmail.com>
2025-02-26 16:56:55 +0400

    сделал кнопку вывода котика, но пока только высылает одну картинку, также сделал кнопки которые пока только визуал

--- bot.py
+++ bot.py
@@ -2,20 +2,27 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot("7676495696:AAFbU3lmZ1o54eH25sWit6pmk_NfA6mS-ok")
-main_buttons = ["Отправь котика"]
+main_buttons = ["Отправь котика", "Инвентарь", "Перезапуск бота"]
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, "Привет, это бот который будет отсылать картинки с котиками по нажатию кнопки. Попробуй!")
+    bot.send_message(message.chat.id, "Привет, это бот который будет отсылать картинки с котиками по нажатию кнопки. Попробуй!", reply_markup=createkeyboard(main_buttons, ))
 
-def createkeyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+def createkeyboard(buttons, row_width=3):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=row_width)
+    keyboardbuttons = []
+    for button in buttons:
+        keyboardbuttons.append(button)
+    keyboard.add(*keyboardbuttons)
     return keyboard
+
 
 @bot.message_handler(func=lambda message: message.text in main_buttons)
 def handlemainbuttonsselection(message):
     if message.text == main_buttons[0]:
-        pass
+        imageurl = "https://steamuserimages-a.akamaihd.net/ugc/1904477344793040639/6BBD7D304F441BFC4EE07FC1A63FEBF939A61955/?imw=512&amp;&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=false"
+        bot.send_photo(message.chat.id, imageurl)
+
 
 
 
