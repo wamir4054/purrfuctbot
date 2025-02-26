50e353287c3dd31354afd412c597c7f87e1d9c15
Senya <wamir4054@gmail.com>
2025-02-26 18:29:58 +0400

    ыыпфп

--- bot.py
+++ bot.py
@@ -16,6 +16,27 @@ def createkeyboard():
 def handlemainbuttonsselection(message):
     if message.text == main_buttons[0]:
         pass
+=======
+main_buttons = ["Отправь котика", "Инвентарь", "Перезапуск бота"]
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Привет, это бот который будет отсылать картинки с котиками по нажатию кнопки. Попробуй!", reply_markup=createkeyboard(main_buttons, ))
+
+def createkeyboard(buttons, row_width=3):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=row_width)
+    keyboardbuttons = []
+    for button in buttons:
+        keyboardbuttons.append(button)
+    keyboard.add(*keyboardbuttons)
+    return keyboard
+
+
+@bot.message_handler(func=lambda message: message.text in main_buttons)
+def handlemainbuttonsselection(message):
+    if message.text == main_buttons[0]:
+        imageurl = "https://steamuserimages-a.akamaihd.net/ugc/1904477344793040639/6BBD7D304F441BFC4EE07FC1A63FEBF939A61955/?imw=512&amp;&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=false"
+        bot.send_photo(message.chat.id, imageurl)
 
 
 
@@ -58,6 +79,10 @@ def handlemainbuttonsselection(message):
 
 
 
+<<<<<<< HEAD
+=======
 
 
+
+>>>>>>> parent of 873c6b3 (Merge branch 'pr/1')
 bot.polling(non_stop=True, interval=0)
